34e89fc6915d552dc711ad69c05f3d3d350e0fa9
jorgegijon91 <jofresme@gmail.com>
2024-10-14 10:39:52 +0200

    Acabamos el loginCorrecto y damos permisos de internet

--- app/src/main/java/com/example/practicatema1/LoginCorrecto.java
+++ app/src/main/java/com/example/practicatema1/LoginCorrecto.java
@@ -1,6 +1,11 @@
 package com.example.practicatema1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -30,5 +35,31 @@ public class LoginCorrecto extends AppCompatActivity {
         //Settear el texto
         textoBienvenido.setText("Bienvenido: " + editTextNombre);
 
+        //Recuperar el imageButton
+        ImageButton imageButton = findViewById(R.id.imageButton);
+
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent actionView y le pasamos la url
+                String url = "“https://www.tutorialspoint.com/android/android_intents_filters.htm";
+                Intent intentUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentUrl);
+
+            }
+        });
+
+        //Recuperar el botón de alarma
+        Button buttonAlarma = findViewById(R.id.buttonAlarma);
+
+        buttonAlarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Pasar a otra actividad
+                Intent intent3 = new Intent(LoginCorrecto.this, CrearAlarma.class);
+            }
+        });
     }
 }
+
